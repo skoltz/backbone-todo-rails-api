33520a92f73219b437a69e9e00698b43147632bd
Stephen Koltz <steve.koltz@gmail.com>
2016-03-19 16:24:56 -0400

    cors.rb added in initializers

--- config/application.rb
+++ config/application.rb
@@ -12,15 +12,10 @@ Bundler.require(*Rails.groups)
 module BackboneTodoRailsApi
   class Application < Rails::Application
 
- config.middleware.insert_before 0, "Rack::Cors" do
-   allow do
-     origins 'localhost:9000'
+    # this is not working for rack-cors. see: https://github.com/cyu/rack-cors
 
-     resource '*',
-       headers: :any,
-       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-   end
- end
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
